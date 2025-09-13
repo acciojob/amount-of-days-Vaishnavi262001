2d20bd942baa154a509d2254e6ffc47aaaa52169
Vaishnavi262001 <164137331+Vaishnavi262001@users.noreply.github.com>
2025-09-13 23:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } 
+	else 
+  {
+    return 365;
+  }
+}
+
+// Prompt user and alert the result
+const year = parseInt(prompt("Enter a year:"));
+alert(daysOfAYear(year))
